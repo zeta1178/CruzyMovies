4e76bab4aa25d8c2d3366894d220be94ecf25ed0
Michael Cruz <michaelcruz@aim.com>
2016-08-24 01:39:16 -0400

    populating the UITableView with a basic array

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+@IBOutlet var tableview: UITableView!
+    
+    //your lifecycle code
+    
+    //temp array
+    let myarray = ["item1", "item2", "item3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
         
+
+        
+        
+        //external json array starts
         
         let requestURL: NSURL = NSURL(string: "http://cruzy.co/movlist5.json")!
         let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: requestURL)
@@ -38,43 +50,83 @@ class ViewController: UIViewController {
                         //for each movie in the moviesArray
                         for movie in movies {
                             
-                          //find the movie name tag
-                         if let mname = movie["mName"] as? String {
-                            
-                            //find the movie thumb tag
-                            if let thumb = movie["mThumb"] as? String {
-                               
+                            //find the movie name tag
+                            if let mname = movie["mName"] as? String {
+                                
                                 //find the movie thumb tag
-                                if let url = movie["mURL"] as? String {
-                                
-                                //print(name,year)
-                                print(mname,thumb,url)
+                                if let thumb = movie["mThumb"] as? String {
                                     
+                                    //find the movie url tag
+                                    if let url = movie["mURL"] as? String {
+                                        
+                                        //print(name,year)
+                                        print(mname,thumb,url)
+                                        
+                                        //temp make array
+                                        //let array2 = [@mName,@mThumb,@mURL]
+                                     
+                                    //end of movie url tag
                                     }
+                                //end of movie thumb tag
                                 }
-                                
+                            //end of movie name tag
                             }
+                        //end of for loop
                         }
-                        
+                    //end of movies array
                     }
-                    
-                }catch {
-                    print("Error with Json: \(error)")
+                //end of do while loop
                 }
                 
+                catch {
+                    print("Error with Json: \(error)")
+                //end of catch
+                }
+            //end of if for JSON download
             }
+        //end of task
         }
         
         task.resume()
         
+        //external json array ends
         
+    //end of viewDidLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    //1 snippet - NumberofRowsInSection Method
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        //To be updated for movie array
+        
+        return myarray.count
+    }
+    //1 snippet end
+    
+    
+    //2nd snippet - cellForRowAtIndexPath
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("customcell", forIndexPath: indexPath)
+        //as! UITableViewCell
+        cell.textLabel?.text = myarray[indexPath.item]
+        return cell
+    }
+    //2nd snippet end
+    
+    //3 snippet
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        tableview.reloadData()
+    }
+    //3 snippet end
 
 }
+
+
 
