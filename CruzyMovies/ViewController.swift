d83d75197b40d0b86c69c5284370cffc36dfac7d
Michael Cruz <michaelcruz@aim.com>
2016-09-16 04:03:40 -0400

    maybe working app

--- CruzyMovies/ViewController.swift
+++ CruzyMovies/ViewController.swift
@@ -19,190 +19,157 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     var TableData:Array< datastruct > = Array < datastruct >()
     
-    enum ErrorHandler:ErrorType
+    enum ErrorHandler:Error
     {
-        case ErrorFetchingResults
+        case errorFetchingResults
     }
     
     struct datastruct
     {
-        
         var dname:String?
         var image:UIImage? = nil
         
         init(add: NSDictionary)
         {
-            
             dname = add["mName"] as? String
         }
     }
     
     
+    @IBOutlet var tableview: UITableView!
     
-   @IBOutlet var tableview: UITableView!
-  
-   override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib
-    
-        //new
+        
         tableview.dataSource = self
         tableview.delegate = self
-    
+        
         get_data_from_url(json_data_url)
-    
-        }
-    
-    
-        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
-            
-            let data = TableData[indexPath.row]
-            
-
-            cell.textLabel?.text = data.dname
-            if (data.image == nil)
-            {
-                cell.imageView?.image = UIImage(named:"image.jpg")
-                load_image(image_base_url + data.dname! + ".jpg", imageview: cell.imageView!, index: indexPath.row)
-            }
-            else
-            {
-                cell.imageView?.image = TableData[indexPath.row].image
-            }
-            
-            return cell
-        }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
-    {
-        showVideo(TableData[indexPath.row].dname!)
+        
     }
     
-    func showVideo(which: String)
-    {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        if let url = NSURL(string: "http://cruzy.co/images/\(which)" + ".mp4")
-            {
-            let vc = SFSafariViewController(URL: url, entersReaderIfAvailable: true)
-            presentViewController(vc, animated: true, completion: nil)
-            
-            }
+        let data = TableData[(indexPath as NSIndexPath).row]
+        
+        cell.textLabel?.text = data.dname
+        if (data.image == nil)
+        {
+            cell.imageView?.image = UIImage(named:"image.jpg")
+            load_image(image_base_url + data.dname! + ".jpg", imageview: cell.imageView!, index: (indexPath as NSIndexPath).row)
+        }
+        else
+        {
+            cell.imageView?.image = TableData[(indexPath as NSIndexPath).row].image
+        }
+        return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        showVideo(TableData[(indexPath as NSIndexPath).row].dname!)
+        //do_table_clear()
+    }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            
-            return TableData.count
+    func showVideo(_ which: String)
+    {
+        
+        if let url = URL(string: "http://cruzy.co/images/\(which)" + ".mp4")
+        {
+            let vc = SFSafariViewController(url: url, entersReaderIfAvailable: true)
+            present(vc, animated: true, completion: nil)
         }
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-    func get_data_from_url(url:String)
-        {
-            
-            
-            let url:NSURL = NSURL(string: url)!
-            let session = NSURLSession.sharedSession()
-            
-            let request = NSMutableURLRequest(URL: url)
-            request.HTTPMethod = "GET"
-            request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
-            
-            
-            let task = session.dataTaskWithRequest(request) {
-                (
-                let data, let response, let error) in
-                
-                guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
-                    print("error")
-                    return
-                }
-                
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.extract_json(data!)
-                    return
-                })
-                
-            }
-            
-            task.resume()
-            
-        }
+        return TableData.count
+    }
+    
+    func get_data_from_url(_ url:String)
+    {
+        let url:URL = URL(string: url)!
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
         
-        
-        func extract_json(jsonData:NSData)
-        {
-            let json: AnyObject?
-            do {
-                json = try NSJSONSerialization.JSONObjectWithData(jsonData, options: [])
-            } catch {
-                json = nil
+        let task = URLSession.shared.dataTask(with: request as URLRequest)
+            {
+            (data, response, error) in
+            
+            guard let _:Data = data, let _:URLResponse = response   , error == nil else {
+                print("error")
                 return
             }
-            
-            if let list = json as? NSArray
-            {
-                for i in (0..<list.count)
-                {
-                    if let data_block = list[i] as? NSDictionary
-                    {
-                        
-                        TableData.append(datastruct(add: data_block))
-                    }
-                }
-                
-                do_table_refresh()
-                
-            }
-            
-            
-        }
-        
-        
-        func do_table_refresh()
-        {
-            dispatch_async(dispatch_get_main_queue(), {
-                self.tableview.reloadData()
+            DispatchQueue.main.async(execute: {
+                self.extract_json(data!)
                 return
             })
         }
-        
-        
-        func load_image(urlString:String, imageview:UIImageView, index:NSInteger)
+        task.resume()
+    }
+    
+    func extract_json(_ jsonData:Data)
+    {
+        let json: AnyObject?
+        do {
+            json = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions()) as AnyObject?
+        } catch {
+            json = nil
+            return
+        }
+        if let list = json as? NSArray
         {
-            
-            let url:NSURL = NSURL(string: urlString)!
-            let session = NSURLSession.sharedSession()
-            
-            let task = session.downloadTaskWithURL(url) {
-                (
-                let location, let response, let error) in
-                
-                guard let _:NSURL = location, let _:NSURLResponse = response  where error == nil else {
-                    print("error")
-                    return
+            for i in (0..<list.count)
+            {
+                if let data_block = list[i] as? NSDictionary
+                {
+                    TableData.append(datastruct(add: data_block))
                 }
-                
-                let imageData = NSData(contentsOfURL: location!)
-                
-                dispatch_async(dispatch_get_main_queue(), {
-                    
-                    self.TableData[index].image = UIImage(data: imageData!)
-                    imageview.image = self.TableData[index].image
-                    return
-                })
-                
-                
+            }
+            do_table_refresh()
+        }
+    }
+   
+    func do_table_refresh()
+    {
+        DispatchQueue.main.async(execute: {
+            self.tableview.reloadData()
+            return
+        })
+    }
+    
+    func load_image(_ urlString:String, imageview:UIImageView, index:NSInteger)
+    {
+        
+        let url:URL = URL(string: urlString)!
+        let session = URLSession.shared
+        
+        let task = session.downloadTask(with: url, completionHandler: {
+            (location, response, error) in
+            
+            guard let _:URL = location, let _:URLResponse = response   , error == nil else {
+                print("error")
+                return
             }
             
-            task.resume()
+            let imageData = try? Data(contentsOf: location!)
             
-            
-        }
- 
-        override func viewDidAppear(animated: Bool) {
-            super.viewDidAppear(animated)
-            tableview.reloadData()
-        }
-    
+            DispatchQueue.main.async(execute: {
+                
+                self.TableData[index].image = UIImage(data: imageData!)
+                imageview.image = self.TableData[index].image
+                return
+            })
+        })
+        task.resume()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableview.reloadData()
+    }
+    
+}
